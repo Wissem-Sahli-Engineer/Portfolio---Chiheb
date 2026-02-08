aa880c601e00d25ff8eb7d815610e64d4894116c
Wissem Sahli <w.g.s.o.m.2003@gmail.com>
2026-02-08 19:39:23 +0100

    updated

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -14,24 +14,5 @@ const IconWrapper = styled.span`
 `;
 
 export default function socialMedia(props) {
-  return (
-    <div className="social-media-div">
-      {socialMediaLinks.map((media, i) => {
-        return (
-          <a
-            key={i}
-            href={media.link}
-            className={`icon-button`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IconWrapper {...media} {...props}>
-              <i className={`fab ${media.fontAwesomeIcon}`}></i>
-            </IconWrapper>
-            {/* <span></span> */}
-          </a>
-        );
-      })}
-    </div>
-  );
+  return null;
 }
